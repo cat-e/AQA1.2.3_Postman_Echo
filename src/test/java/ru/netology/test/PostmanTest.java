bcc616f31d9db95b8b2234fef15c051433c17969
cat-e <khlebalova.e@yandex.ru>
2020-08-18 23:44:36 +0400

    initial commit

---
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+    void shouldPlaceData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+           .when()
+                .post("/post")
+           .then()
+                .statusCode(200)
+                .body("data", equalTo("some data1"))
+        ;
+    }
+
+
+}
