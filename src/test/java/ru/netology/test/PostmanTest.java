674387e79786b320f3425b15d64efc33f9bc7bf9
cat-e <khlebalova.e@yandex.ru>
2020-08-19 01:20:06 +0400

    fix response in body

--- src/test/java/ru/netology/test/PostmanTest.java
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -16,7 +16,7 @@ class PostmanTest {
                 .post("/post")
            .then()
                 .statusCode(200)
-                .body("data", equalTo("some data1"))
+                .body("data", equalTo("some data"))
         ;
     }
 
